6d031879216f8e430cdff559df8d7207240f5f32
Ben <ben@crimson.net.nz>
2012-03-24 17:50:44 +1300

    Changing the positions of items on Submit report, including making optional fields more obvious

--- views/reports_submit.php
+++ views/reports_submit.php
@@ -114,42 +114,10 @@
 						?>
 					</div>
 				</div>
-
-
 				<?php
 				// Action::report_form - Runs right after the report categories
 				Event::run('ushahidi_action.report_form');
 				?>
-
-				<?php echo $custom_forms ?>
-
-				<div class="report_optional">
-					<h3><?php echo Kohana::lang('ui_main.reports_optional'); ?></h3>
-					<div class="report_row">
-						<h4><?php echo Kohana::lang('ui_main.reports_first'); ?></h4>
-						<?php print form::input('person_first', $form['person_first'], ' class="text long"'); ?>
-					</div>
-					<div class="report_row">
-						<h4><?php echo Kohana::lang('ui_main.reports_last'); ?></h4>
-						<?php print form::input('person_last', $form['person_last'], ' class="text long"'); ?>
-					</div>
-					<div class="report_row">
-						<h4><?php echo Kohana::lang('ui_main.reports_email'); ?></h4>
-						<?php print form::input('person_email', $form['person_email'], ' class="text long"'); ?>
-					</div>
-					<?php
-					// Action::report_form_optional - Runs in the optional information of the report form
-					Event::run('ushahidi_action.report_form_optional');
-					?>
-				</div>
-			</div>
-			<div class="report_right">
-				<?php if ( ! $multi_country AND count($cities) > 1){ ?>
-				<div class="report_row">
-					<h4><?php echo Kohana::lang('ui_main.reports_find_location'); ?></h4>
-					<?php print form::dropdown('select_city',$cities,'', ' class="select" '); ?>
-				</div>
-				<?php } ?>
 				<div class="report_row">
 					<div id="divMap" class="report_map">
 						<div id="geometryLabelerHolder" class="olControlNoSelect">
@@ -190,123 +158,154 @@
 					<?php print form::input('location_name', $form['location_name'], ' class="text long"'); ?>
 				</div>
 
-				<!-- News Fields -->
-				<div id="divNews" class="report_row">
-					<h4><?php echo Kohana::lang('ui_main.reports_news'); ?></h4>
-					<?php
-						$this_div = "divNews";
-						$this_field = "incident_news";
-						$this_startid = "news_id";
-						$this_field_type = "text";
-						if (empty($form[$this_field]))
-						{
-							$i = 1;
-							print "<div class=\"report_row\">";
-							print form::input($this_field . '[]', '', ' class="text long2"');
-							print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
-							print "</div>";
-						}
-						else
-						{
-							$i = 0;
-							foreach ($form[$this_field] as $value) {
-							print "<div class=\"report_row\" id=\"$i\">\n";
-
-							print form::input($this_field . '[]', $value, ' class="text long2"');
-							print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
-							if ($i != 0)
-							{
-								print "<a href=\"#\" class=\"rem\"	onClick='removeFormField(\"#" . $this_field . "_" . $i . "\"); return false;'>remove</a>";
-							}
-							print "</div>\n";
-							$i++;
-						}
-					}
-					print "<input type=\"hidden\" name=\"$this_startid\" value=\"$i\" id=\"$this_startid\">";
-				?>
-				</div>
-
-
-				<!-- Video Fields -->
-				<div id="divVideo" class="report_row">
-					<h4><?php echo Kohana::lang('ui_main.reports_video'); ?></h4>
-					<?php
-						$this_div = "divVideo";
-						$this_field = "incident_video";
-						$this_startid = "video_id";
-						$this_field_type = "text";
-
-						if (empty($form[$this_field]))
-						{
-							$i = 1;
-							print "<div class=\"report_row\">";
-							print form::input($this_field . '[]', '', ' class="text long2"');
-							print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
-							print "</div>";
-						}
-						else
-						{
-							$i = 0;
-							foreach ($form[$this_field] as $value) {
-								print "<div class=\"report_row\" id=\"$i\">\n";
-
-								print form::input($this_field . '[]', $value, ' class="text long2"');
-								print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
-								if ($i != 0)
-								{
-									print "<a href=\"#\" class=\"rem\"	onClick='removeFormField(\"#" . $this_field . "_" . $i . "\"); return false;'>remove</a>";
-								}
-								print "</div>\n";
-								$i++;
-							}
-						}
-						print "<input type=\"hidden\" name=\"$this_startid\" value=\"$i\" id=\"$this_startid\">";
-					?>
-				</div>
-
-				<!-- Photo Fields -->
-				<div id="divPhoto" class="report_row">
-					<h4><?php echo Kohana::lang('ui_main.reports_photos'); ?></h4>
-					<?php
-						$this_div = "divPhoto";
-						$this_field = "incident_photo";
-						$this_startid = "photo_id";
-						$this_field_type = "file";
-
-						if (empty($form[$this_field]['name'][0]))
-						{
-							$i = 1;
-							print "<div class=\"report_row\">";
-							print form::upload($this_field . '[]', '', ' class="file long2"');
-							print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
-							print "</div>";
-						}
-						else
-						{
-							$i = 0;
-							foreach ($form[$this_field]['name'] as $value) 
-							{
-								print "<div class=\"report_row\" id=\"$i\">\n";
-
-								// print "\"<strong>" . $value . "</strong>\"" . "<BR />";
-								print form::upload($this_field . '[]', $value, ' class="file long2"');
-								print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
-								if ($i != 0)
-								{
-									print "<a href=\"#\" class=\"rem\"	onClick='removeFormField(\"#" . $this_field . "_" . $i . "\"); return false;'>remove</a>";
-								}
-								print "</div>\n";
-								$i++;
-							}
-						}
-						print "<input type=\"hidden\" name=\"$this_startid\" value=\"$i\" id=\"$this_startid\">";
-					?>
-
-				</div>
-									
 				<div class="report_row">
 					<input name="submit" type="submit" value="<?php echo Kohana::lang('ui_main.reports_btn_submit'); ?>" class="btn_submit" /> 
 				</div>
+
+				<?php echo $custom_forms ?>
+
+			</div>
+			<div class="report_right">
+				<div class="report_optional">
+					<h3><?php echo Kohana::lang('ui_main.reports_optional'); ?></h3>
+					<div class="report_row">
+						<h4><?php echo Kohana::lang('ui_main.reports_first'); ?></h4>
+						<?php print form::input('person_first', $form['person_first'], ' class="text long"'); ?>
+					</div>
+					<div class="report_row">
+						<h4><?php echo Kohana::lang('ui_main.reports_last'); ?></h4>
+						<?php print form::input('person_last', $form['person_last'], ' class="text long"'); ?>
+					</div>
+					<div class="report_row">
+						<h4><?php echo Kohana::lang('ui_main.reports_email'); ?></h4>
+						<?php print form::input('person_email', $form['person_email'], ' class="text long"'); ?>
+					</div>
+					<?php
+					// Action::report_form_optional - Runs in the optional information of the report form
+					Event::run('ushahidi_action.report_form_optional');
+					?>
+          <?php if ( ! $multi_country AND count($cities) > 1){ ?>
+          <div class="report_row">
+            <h4><?php echo Kohana::lang('ui_main.reports_find_location'); ?></h4>
+            <?php print form::dropdown('select_city',$cities,'', ' class="select" '); ?>
+          </div>
+          <?php } ?>
+
+          <!-- News Fields -->
+          <div id="divNews" class="report_row">
+            <h4><?php echo Kohana::lang('ui_main.reports_news'); ?></h4>
+            <?php
+              $this_div = "divNews";
+              $this_field = "incident_news";
+              $this_startid = "news_id";
+              $this_field_type = "text";
+              if (empty($form[$this_field]))
+              {
+                $i = 1;
+                print "<div class=\"report_row\">";
+                print form::input($this_field . '[]', '', ' class="text long2"');
+                print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
+                print "</div>";
+              }
+              else
+              {
+                $i = 0;
+                foreach ($form[$this_field] as $value) {
+                print "<div class=\"report_row\" id=\"$i\">\n";
+
+                print form::input($this_field . '[]', $value, ' class="text long2"');
+                print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
+                if ($i != 0)
+                {
+                  print "<a href=\"#\" class=\"rem\"	onClick='removeFormField(\"#" . $this_field . "_" . $i . "\"); return false;'>remove</a>";
+                }
+                print "</div>\n";
+                $i++;
+              }
+            }
+            print "<input type=\"hidden\" name=\"$this_startid\" value=\"$i\" id=\"$this_startid\">";
+          ?>
+          </div>
+
+
+          <!-- Video Fields -->
+          <div id="divVideo" class="report_row">
+            <h4><?php echo Kohana::lang('ui_main.reports_video'); ?></h4>
+            <?php
+              $this_div = "divVideo";
+              $this_field = "incident_video";
+              $this_startid = "video_id";
+              $this_field_type = "text";
+
+              if (empty($form[$this_field]))
+              {
+                $i = 1;
+                print "<div class=\"report_row\">";
+                print form::input($this_field . '[]', '', ' class="text long2"');
+                print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
+                print "</div>";
+              }
+              else
+              {
+                $i = 0;
+                foreach ($form[$this_field] as $value) {
+                  print "<div class=\"report_row\" id=\"$i\">\n";
+
+                  print form::input($this_field . '[]', $value, ' class="text long2"');
+                  print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
+                  if ($i != 0)
+                  {
+                    print "<a href=\"#\" class=\"rem\"	onClick='removeFormField(\"#" . $this_field . "_" . $i . "\"); return false;'>remove</a>";
+                  }
+                  print "</div>\n";
+                  $i++;
+                }
+              }
+              print "<input type=\"hidden\" name=\"$this_startid\" value=\"$i\" id=\"$this_startid\">";
+            ?>
+          </div>
+
+          <!-- Photo Fields -->
+          <div id="divPhoto" class="report_row">
+            <h4><?php echo Kohana::lang('ui_main.reports_photos'); ?></h4>
+            <?php
+              $this_div = "divPhoto";
+              $this_field = "incident_photo";
+              $this_startid = "photo_id";
+              $this_field_type = "file";
+
+              if (empty($form[$this_field]['name'][0]))
+              {
+                $i = 1;
+                print "<div class=\"report_row\">";
+                print form::upload($this_field . '[]', '', ' class="file long2"');
+                print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
+                print "</div>";
+              }
+              else
+              {
+                $i = 0;
+                foreach ($form[$this_field]['name'] as $value) 
+                {
+                  print "<div class=\"report_row\" id=\"$i\">\n";
+
+                  // print "\"<strong>" . $value . "</strong>\"" . "<BR />";
+                  print form::upload($this_field . '[]', $value, ' class="file long2"');
+                  print "<a href=\"#\" class=\"add\" onClick=\"addFormField('$this_div','$this_field','$this_startid','$this_field_type'); return false;\">add</a>";
+                  if ($i != 0)
+                  {
+                    print "<a href=\"#\" class=\"rem\"	onClick='removeFormField(\"#" . $this_field . "_" . $i . "\"); return false;'>remove</a>";
+                  }
+                  print "</div>\n";
+                  $i++;
+                }
+              }
+              print "<input type=\"hidden\" name=\"$this_startid\" value=\"$i\" id=\"$this_startid\">";
+            ?>
+
+          </div>
+				</div>
+									
 			</div>
 		</div>
 		<?php print form::close(); ?>
